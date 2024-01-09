6c790c57341477df3b12123307f8db2c49be8274
MartinBallay <ballay.martin307@gmail.com>
2024-01-09 13:18:46 +0100

    dokoncenie contact us

--- ballay_projekt/src/views/Contact-Us.vue
+++ ballay_projekt/src/views/Contact-Us.vue
@@ -1,9 +1,11 @@
 <script setup>
 import contactUsBanner from '@/components/contact-us-banner.vue';
+import contactUsMainPage from '@/components/contact-us-main-page.vue'
 </script>
 
 <template>
    <contactUsBanner></contactUsBanner>
+   <contactUsMainPage></contactUsMainPage>
 </template>
 
 <style>
